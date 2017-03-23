c8494f8f4afd9dbeb145f7a1e8f29fe2333da117
Maisu <mystasly48@gmail.com>
2017-03-24 03:10:35 +0900

    support to multi display

--- RotateScreenByShortcut/Form1.cs
+++ RotateScreenByShortcut/Form1.cs
@@ -19,31 +19,24 @@ namespace RotateScreenByShortcut {
     }
 
     private void hook_KeyPressed(object sender, KeyPressedEventArgs e) {
-      uint displayIndex = 1;
+      uint displayIndex = (uint)(Array.IndexOf(Screen.AllScreens, Screen.FromPoint(Cursor.Position)) + 1);
       Debug.WriteLine("Key: {0}, Display: {1}", e.Key, displayIndex);
-
       switch (e.Key) {
         case Keys.Left:
           RotateDisplay.Rotate(displayIndex, DisplayOrientations.DEGREES_CW_270);
-          //RotateDisplay.Left();
           break;
         case Keys.Right:
           RotateDisplay.Rotate(displayIndex, DisplayOrientations.DEGREES_CW_90);
-          //RotateDisplay.Right();
           break;
         case Keys.Up:
           RotateDisplay.Rotate(displayIndex, DisplayOrientations.DEGREES_CW_0);
-          //RotateDisplay.Up();
           break;
         case Keys.Down:
           RotateDisplay.Rotate(displayIndex, DisplayOrientations.DEGREES_CW_180);
-          //RotateDisplay.Down();
           break;
       }
     }
 
-    private void Form1_Shown(object sender, EventArgs e) {
-      Hide();
-    }
+    private void Form1_Shown(object sender, EventArgs e) => Hide();
   }
 }
